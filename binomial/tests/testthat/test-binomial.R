3d1a850c310d5db381417cea08d238ae81c69423
Ray Yuan <ray.x.yuan@gmail.com>
2019-05-03 22:39:57 -0700

    final commit

--- binomial/tests/testthat/test-binomial.R
+++ binomial/tests/testthat/test-binomial.R
@@ -1,33 +1,32 @@
 context("Test Binomial Functions")
 
-test_that("testing bin choose", {
+test_that("test bin choose", {
   expect_equal(bin_choose(5,2), 10)
-
-  expect_equal(bin_choose(5, 1:5), c(5, 10, 10,  5,  1))
+  expect_length(bin_choose(5, 2), 1)
+  expect_equal(bin_choose(12, c(10, 11)), c(66, 12))
   expect_error(bin_choose(5,6))
   expect_error(bin_choose(5, 1:6))
 })
 
-test_that("testing bin probability", {
-  expect_equal(bin_probability(success = 2, trials = 5, prob = 0.5), 0.3125)
-  expect_equal(bin_probability(success = 0:2, trials = 5, prob = 0.5), c(0.03125, 0.15625, 0.31250))
-  expect_equal(bin_probability(success = 55, trials = 100, prob = 0.45), 0.01075277)
-  expect_error(bin_probability(success = 6, trials = 5, prob = 0.5))
-  expect_error(bin_probability(success = 0:5 , trials = 5, prob = 0.5))
+test_that("test bin probability", {
+  expect_error(bin_probability(2, 10, 2))
+  expect_error(bin_probability(2, -10, 0.5))
+  expect_length(bin_probability(55, 100, 0.45), 1)
+  expect_equal(bin_probability(2, 5,0.5), 0.3125)
 })
 
-test_that("testing bin distribution", {
-  expect_equal(bin_distribution(50, 0.2)$probability[3], 0.001092737)
-  expect_equal(bin_distribution(72, 0.74)$probability[3], 1.20856e-13)
-  expect_equal(bin_distribution(72, 0.74)$probability[62], 0.01169977)
-  expect_equal(bin_distribution(152, 0.4)$probability[100], 2.305374e-10)
+test_that("test bin distribution", {
   expect_s3_class(bin_distribution(50, 0.2), "bindis")
   expect_s3_class(bin_distribution(50, 0.2), "data.frame")
+  expect_error(bin_distribution(-10, 0.5))
+  expect_length(bin_distribution(8, 0.5), 2)
+  expect_equal(bin_distribution(50, 0.2)$probability[3], 0.001092737)
 })
 
-test_that("testing bin cumulative", {
-  expect_equal(bin_cumulative(trials = 5, prob = 0.5)$cumulative[2], 0.18750)
-  expect_equal(bin_cumulative(trials = 10, prob = 0.2)$cumulative[4], (0.8791261 + 1.84e-08))
-  expect_s3_class(bin_cumulative(trials = 5, prob = 0.5), "bincum")
+test_that("test bin cumulative", {
+  expect_error(bin_cumulative(5, -0.5))
   expect_s3_class(bin_cumulative(trials = 5, prob = 0.5), "data.frame")
+  expect_s3_class(bin_cumulative(trials = 5, prob = 0.5), "bincum")
+  expect_equal(bin_cumulative(trials = 5, prob = 0.5)$cumulative[2], 0.18750)
+
 })
--- binomial/tests/testthat/test-summary-measures.R
+++ binomial/tests/testthat/test-summary-measures.R
@@ -1,31 +1,31 @@
-context("Test Summary Measures")
+context("Test the summary measures")
 
-test_that("testing aux mean", {
+test_that("aux mean test", {
   expect_equal(aux_mean(10, 0.3), 3)
   expect_equal(aux_mean(100,0.3),30)
   expect_equal(aux_mean(0,5),0)
 })
 
-test_that("testing aux variance", {
-  expect_equal(length(aux_variance(30, 0.3)), 6.3)
+test_that("aux variance test", {
+  expect_equal(aux_variance(30, 0.3), 6.3)
   expect_equal(aux_variance(10, 0.3), 2.1)
   expect_equal(aux_variance(20, 0.3), 4.2)
 })
 
-test_that("testing aux mode", {
+test_that("aux mode test", {
   expect_equal(aux_mode(10, 0.3), 3)
-  expect_equal(aux_mode(10, 0.35), 3)
-  expect_equal(aux_mode(10, 0.39), 4)
-  expect_equal(aux_mode(10, 0.4), 4)
+  expect_equal(aux_mode(10, 0.33), 3)
+  expect_equal(aux_mode(10, 0.37), 4)
+  expect_equal(aux_mode(10, 0.41), 4)
 })
 
-test_that("testing aux skewness", {
+test_that("aux skewness test", {
   expect_equal(round(aux_skewness(10, 0.3), 5), 0.27603)
   expect_equal(round(aux_skewness(25, 0.4), 5), 0.08165)
   expect_equal(round(aux_skewness(50, 0.1), 5), 0.37712)
 })
 
-test_that("testing aux kurtosis", {
+test_that("aux kurtosis test", {
   expect_equal(round(aux_kurtosis(10, 0.3), 5), -0.12381)
   expect_equal(round(aux_kurtosis(20, 0.4), 5), -0.09167)
   expect_equal(round(aux_kurtosis(50, 0.2), 5), 0.005)
